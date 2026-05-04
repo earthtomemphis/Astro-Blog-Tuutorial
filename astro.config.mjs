bb7017ba4263e898696ce86411cde6e56c2d3781
Memphis DiAngelis <earthtomemphis@gmail.com>
2026-05-04 20:24:07 +0800

    added rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://69f88f1bd627f18957e6bfd2--scintillating-dasik-836445.netlify.app/',
+});
